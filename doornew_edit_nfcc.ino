7b8125cdef78c88ffad813679148a41d99115541
Athul2711 <57836199+Athul2711@users.noreply.github.com>
2019-11-19 18:54:33 +0530

    Update doornew_edit_nfcc.ino

--- doornew_edit_nfcc.ino
+++ doornew_edit_nfcc.ino
@@ -131,8 +131,8 @@ void repeatMe() {
   byte letter;
   for (byte i = 0; i < mfrc522.uid.size; i++) 
   {
-     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-     Serial.print(mfrc522.uid.uidByte[i], HEX);
+     Terminal.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+     Terminal.print(mfrc522.uid.uidByte[i], HEX);
      content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
      content.concat(String(mfrc522.uid.uidByte[i], HEX));
   }
